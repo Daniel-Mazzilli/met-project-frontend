3dcfb149da46b721f1a9fbaabf1f0a2e3d9b3290
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-08-25 11:32:37 -0400

    working on navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,20 @@
-import Hamburger from "../../assets/hamburger_light.png"
-import "./Navbar.scss"
+import { useState } from "react";
+import Hamburger from "../../assets/hamburger_light.png";
+import "./Navbar.scss";
 
-export default function Navbar (){
-    return(
-        <nav className="navbar">
-            <img className="navbar__img" src={Hamburger} alt="hamburger menu icon"/>
-            MET Explorer
-        </nav>
-    )
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <nav className="navbar">
+      <img className="navbar__img" onClick={() => setIsMenuOpen(true)} src={Hamburger} alt="hamburger menu icon" />
+      MET Explorer
+      {isMenuOpen && <div className="navbar__open">
+        <div className="navbar__open__content">
+            Hello
+        </div>
+        <div className="navbar__open__cover" onClick={() => setIsMenuOpen(false)}> 
+        </div>
+      </div>}
+    </nav>
+  );
 }
