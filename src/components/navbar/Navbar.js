27b9d59106ba3a2191d20d8cd2d0968f9952a2b8
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-08-25 11:09:57 -0400

    working on small card and navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,10 @@
+import Hamburger from "../../assets/hamburger_light.png"
 import "./Navbar.scss"
 
 export default function Navbar (){
     return(
         <nav className="navbar">
+            <img className="navbar__img" src={Hamburger} alt="hamburger menu icon"/>
             MET Explorer
         </nav>
     )
