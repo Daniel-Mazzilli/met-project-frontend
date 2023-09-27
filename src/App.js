1aa34911a27e8b090c712ab1d6f95e30a1d50a9d
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-09-27 14:07:40 -0400

    added scroll block, minor nav and footer adjustments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useContextProvider } from "./providers/Provider.js";
 import RouteComponent from "./components/routes/RouteComponent.js";
 import "./App.scss";
 
 function App() {
+  const {isMenuOpen} = useContextProvider();
+
+  //scroll lock when nav menu is open
+  useEffect(() => {
+    if(isMenuOpen){
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [isMenuOpen])
+
   return (
     <div className="App">
       <RouteComponent />
