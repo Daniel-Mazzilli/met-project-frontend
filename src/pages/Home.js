06d2b20897f5ea5b15a21911d0f9db6e7db6cf90
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-09-27 17:48:21 -0400

    to top button adjustment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <div>
-            IN CONSTRUCTION
+            IN DEVELOPMENT
             <br/>
             <br/>
-            currently working on <Link to="search" style={{color: "blue"}}>search page</Link>
+            currently working on the <Link to="search" style={{color: "blue"}}>search</Link>
         </div>
     );
 }
